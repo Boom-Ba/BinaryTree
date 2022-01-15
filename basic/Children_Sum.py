2acd5e1606655a2fe280aec6241030fe5a8b5395
Boom-Ba <70102254+Boom-Ba@users.noreply.github.com>
2022-01-15 09:42:01 -0500

    Create Children_Sum.py

---
+++ basic/Children_Sum.py
@@ -0,0 +1,15 @@
+def checkChildrenSumProperty(self, root: Node) -> bool:
+		# Write your code here...
+		def solve(root):
+			if not root:
+				return True, 0
+			if not root.left and not root.right:
+				return True, root.data
+			left,left_sum =solve(root.left)
+			right,right_sum =solve(root.right)
+			if left_sum!=float('inf') and right_sum!=float('inf') and left_sum+right_sum ==root.data:
+				return True, root.data
+			else:
+				return False, float('inf')
+
+		return solve(root)[0]

de7a414a9d55b7048de8fd0bbc67c1cee7d76646
Boom-Ba <70102254+Boom-Ba@users.noreply.github.com>
2022-01-19 14:24:06 -0500

    Create Only_PostOrder_Construct.py

---
+++ Construct/Only_PostOrder_Construct.py
@@ -0,0 +1,27 @@
+"""
+Use bound to recursively construct LST and RST
+"""
+
+def constructBST(self, postorder: List[int]) -> Node:
+		def construct(start, end ):
+			if start>end:
+				return None 
+				
+			val =postorder[end]
+			root =Node(val)
+			end-=1
+			index=end
+			while index>=0:
+				if postorder[index]<val:
+					break
+				index-=1
+				
+			root.left= construct(start, index)
+			root.right =construct(index+1, end)
+			
+			return root 
+		
+		if not postorder:
+			return None 
+			
+		return construct( 0, len(postorder)-1 )

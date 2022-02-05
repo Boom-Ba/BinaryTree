f6aa4b7a5fa46f7a6ccfcb94d7ae8f1c231ce608
Boom-Ba <70102254+Boom-Ba@users.noreply.github.com>
2022-02-05 16:47:41 -0500

    Create printPath.py

---
+++ Path/printPath.py
@@ -0,0 +1,19 @@
+def helper(root,path,res):
+  if not root:
+    return 
+  #backtrack
+  path.append(root.val)
+  if not root.left and not root.right:
+    res.append(path[:])
+
+  helper(root.left,path,res)
+  helper(root.right,path,res)
+  path.pop()
+
+  
+def printPath(root):
+  res=[]
+  if not root:
+    return res
+  helper(root,[],res)
+  return res
